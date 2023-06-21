a1333b824d4cdf31f068b2d91fb55ebb14f77b2b
Cyryl <folkbread@gmail.com>
2023-06-21 15:25:50 +0200

    refactor: changed valure for product in mobile device

--- src/components/ProductsList/ProductsList.tsx
+++ src/components/ProductsList/ProductsList.tsx
@@ -4,7 +4,7 @@ import ProductCard from "../ProductCard";
 
 const ProductsList = () => {
   return (
-    <div className="flex flex-wrap w-full lg:gap-10 max-md:justify-between max-lg:justify-evenly lg:justify-center xl:justify-left">
+    <div className="flex flex-wrap w-full lg:gap-[1.80rem] max-sm:justify-between max-lg:justify-evenly lg:justify-left">
       {products.map((prod) => {
         return (
           <ProductCard
@@ -15,13 +15,13 @@ const ProductsList = () => {
             priceDiscount={prod?.priceDiscount}
             priceOld={prod?.priceOld}
             key={prod?.name}
-            classNameContainer="max-sm:p-[0.75rem] max-sm:max-w-[160px] max-sm:h-[320px] max-md:max-w-[200px] max-md:h-[380px]"
-            classNameCategory="max-md:max-w-full max-md:h-[20px]"
-            classNameTitle="max-md:text-base"
-            classNameContainerPrice="max-md:flex-col"
+            classNameContainer="max-sm:p-[0.75rem] max-sm:max-w-[130px] max-sm:h-[290px] max-md:max-w-[200px] max-md:h-[380px]"
+            classNameCategory="max-sm:text-xs max-md:max-w-full max-md:h-[20px]"
+            classNameTitle="max-sm:text-sm max-md:text-base"
+            classNameContainerPrice=" max-sm:gap-1 max-md:flex-col"
             classNameActualPrice="max-md:text-lg"
             classNameRating="max-md:ml-0"
-            classNameDivider="max-sm:mt-[0.5rem]"
+            classNameDivider="max-sm:mt-auto"
           />
         );
       })}
