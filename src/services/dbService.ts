eb638f05bbea2406475c6b0b9b6adb6c8d19cdbc
Kiryl Shchasny <folkbread@gmail.com>
2023-07-24 19:05:33 +0200

    feat: added functions for work with database

--- src/services/dbService.ts
+++ src/services/dbService.ts
@@ -14,6 +14,80 @@ class DbService extends SupabaseClientService {
     super();
   }
 
+  async onRepeatFileInStorage(bucket: string, subfolder: string, file: File) {
+    try {
+      const isRepeat: any[] = (
+        await this.supabase.storage.from(bucket).list(subfolder)
+      ).data.filter(item => item.name === file.name);
+
+      if (isRepeat.length > 0) {
+        return {data: {isRepeat: true, repeatFiles: isRepeat}, error: null};
+      }
+
+      return {data: {isRepeat: false, repeatFiles: isRepeat}, error: null};
+    } catch (error) {
+      return {
+        data: null,
+        error: {
+          name: error?.name,
+          message: error?.message,
+        },
+      };
+    }
+  }
+
+  async onRemoveFileInStorage(bucket: string, path: string) {
+    try {
+      await this.supabase.storage.from(bucket).remove([path]);
+    } catch (error) {
+      return {
+        name: error?.name,
+        message: error?.message,
+      };
+    }
+  }
+
+  async onUploadFileToStorage(bucket: string, path: string, file: File) {
+    try {
+      const {data, error} = await this.supabase.storage
+        .from(bucket)
+        .upload(path, file);
+
+      if (data.path) {
+        return {data, error: null};
+      }
+
+      return {data: null, error};
+    } catch (error) {
+      return {
+        data: null,
+        error: {
+          name: error?.name,
+          message: error?.message,
+        },
+      };
+    }
+  }
+
+  async onUpdateDataInTable(
+    table: string,
+    field: string,
+    value: string | null,
+    id: string,
+  ) {
+    try {
+      await this.supabase
+        .from(table)
+        .update({[field]: value})
+        .eq('id', id);
+    } catch (error) {
+      return {
+        name: error?.name,
+        message: error?.message,
+      };
+    }
+  }
+
   async onSelectUserFromDB(id: string) {
     const {data: users, error} = await this.supabase
       .from('users')
